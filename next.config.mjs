7ce16a502c4f51db0dd549f27ea93e02bf630add
Nimai Dhara <thecharan25@gmail.com>
2026-05-23 14:03:16 +0530

    Updated scraper with proxy and fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  serverExternalPackages: ['puppeteer-core', '@sparticuz/chromium'],
 };
 
 export default nextConfig;
